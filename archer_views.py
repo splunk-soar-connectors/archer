2f6bf9cb5d65a37d52de2fb22479bd6d9bf07401
gary.jeon <gjeon@splunk.com>
2018-10-19 13:56:47 -0700

    PAPP-5204 archer - remove unicode dash from the copyright info

--- archer_views.py
+++ archer_views.py
@@ -1,8 +1,10 @@
 # File: archer_views.py
 # Copyright (c) 2016-2018 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
 
 from django.http import HttpResponse
 import json
