7de72fddfe868d894a2f917b5fd8dbc48f84088c
dhwanis-crest <dhwanis@splunk.com>
2024-03-21 15:59:49 +0530

    dev check changes

--- archer_connector.py
+++ archer_connector.py
@@ -998,22 +998,67 @@ class ArcherConnector(BaseConnector):
 
 
 if __name__ == '__main__':
-    from traceback import format_exc
+    import argparse
 
     import pudb
+    import requests
     pudb.set_trace()
-    if (len(sys.argv) < 2):
-        print('No test json specified as input')
-        sys.exit(0)
-    with open(sys.argv[1]) as f:
+
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+    verify = args.verify
+
+    if (username is not None and password is None):
+        # User specified a username but not a password, so ask
+        import getpass
+        password = getpass.getpass("Password: ")
+
+    if (username and password):
+        try:
+            login_url = BaseConnector._get_phantom_base_url()
+
+            print("Accessing the Login page")
+            r = requests.get(BaseConnector._get_phantom_base_url() + "login", verify=verify)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken=' + csrftoken
+            headers['Referer'] = BaseConnector._get_phantom_base_url() + 'login'
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=verify, data=data, headers=headers, timeout=consts.DEFAULT_TIMEOUT)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print("Unable to get session id from the platfrom. Error: " + str(e))
+            sys.exit(1)
+
+    with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=4))
         connector = ArcherConnector()
         connector.print_progress_message = True
-        try:
-            ret_val = connector._handle_action(json.dumps(in_json), None)
-        except:
-            print(format_exc())
+
+        if (session_id is not None):
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
+        ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
+
     sys.exit(0)
--- archer_consts.py
+++ archer_consts.py
@@ -62,3 +62,4 @@ ARCHER_STATE_FILE_CORRUPT_ERR = (
     "Error occurred while loading the state file due to its unexpected format. "
     "Resetting the state file with the default format. Please try again."
 )
+DEFAULT_TIMEOUT = 30
